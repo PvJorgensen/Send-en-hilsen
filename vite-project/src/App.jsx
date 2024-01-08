0db6abf1aaf6553140920f726aee0592f986e4be
Patrik_Jørgensen <vadmand806@gmail.com>
2024-01-08 12:56:56 +0100

    update

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,15 @@
 import { Name } from "../components/Names/Name.jsx"
+import { List } from "../components/Favmeal/list.jsx"
+const arrItems = [
+  'Pizza', 'Boller i karry', 'Paste med kylling og pesto', 'con carne', 'Beef Wellington'
+]
 
 function App() {
 
   return (
     <>
     <Name Name="Frank"></Name>
+    <List data={arrItems}></List>
     </>
   )
 }
